09064ed29007e38bc20220eda30029974454be5d
joeface71 <joefacenda@yahoo.com>
2022-06-19 07:52:21 -0400

    First commit

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,100 @@
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public int width;
+    public int height;
+
+    public int borderSize;
+
+    public GameObject tilePrefab;
+    public GameObject[] gamePiecePrefabs;
+
+    Tile[,] m_allTiles;
+    GamePiece[,] m_allGamePieces;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_allTiles = new Tile[width, height];
+        m_allGamePieces = new GamePiece[width, height];
+
+        SetupTiles();
+        SetupCamera();
+        FillRandom();
+    }
+
+    void SetupTiles()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                GameObject tile = Instantiate(tilePrefab, new Vector3(i, j, 0), Quaternion.identity) as GameObject;
+
+                tile.name = "Tile (" + i + "," + j + ")";
+
+                m_allTiles[i, j] = tile.GetComponent<Tile>();
+
+                tile.transform.parent = transform;
+
+                m_allTiles[i, j].Init(i, j, this);
+            }
+        }
+    }
+
+    void SetupCamera()
+    {
+        Camera.main.transform.position = new Vector3((float)(width - 1) / 2f, (float)(height - 1) / 2f, -10f); // cast ints as float otherwise you could lose data
+
+        // determine whether to use height or width to determine orthographic size
+        float aspectRatio = (float)Screen.width / (float)Screen.height;
+        float verticalSize = (float)height / 2f + (float)borderSize;
+        float horizontalSize = ((float)width / 2f + (float)borderSize) / aspectRatio;
+
+        Camera.main.orthographicSize = (verticalSize > horizontalSize) ? verticalSize : horizontalSize;
+    }
+
+    GameObject GetRandomGamePiece()
+    {
+        int randomIndex = Random.Range(0, gamePiecePrefabs.Length);
+
+        if (gamePiecePrefabs[randomIndex] == null)
+        {
+            Debug.LogWarning("Board: " + randomIndex + " does not contain a valid GamePiece prefab");
+        }
+
+        return gamePiecePrefabs[randomIndex];
+    }
+
+    void PlaceGamePiece(GamePiece gamePiece, int x, int y)
+    {
+        if (gamePiece == null)
+        {
+            Debug.LogWarning("Board: Invalid GamePiece");
+            return;
+        }
+
+        gamePiece.transform.position = new Vector3(x, y, 0);
+        gamePiece.transform.rotation = Quaternion.identity;
+        gamePiece.SetCoord(x, y);
+    }
+
+    void FillRandom()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                GameObject randomPiece = Instantiate(GetRandomGamePiece(), Vector3.zero, Quaternion.identity) as GameObject;
+
+                if (randomPiece != null)
+                {
+                    PlaceGamePiece(randomPiece.GetComponent<GamePiece>(), i, j);
+                }
+            }
+        }
+    }
+
+
+}
